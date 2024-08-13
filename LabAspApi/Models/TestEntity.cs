7f7df241d0aebab8b53b36f5a75e8a7367d5ba1a
2PECTRE <iam2pectre@gmail.com>
2024-08-13 10:26:31 +0900

    2408131026

--- LabAspApi/Models/TestEntity.cs
+++ LabAspApi/Models/TestEntity.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace LabAspApi.Models
 {
     public class TestEntity
     {
-        
+        public int Id { get; set; }
+        public string Test { get; set; }
     }
 }
